ea456b1f2d183307914c1eee985d8ea84d281c09
olepavlsen <olepavlsen@mail.ru>
2024-07-12 16:59:55 +0300

    Add files via upload

--- homework2.py
+++ homework2.py
@@ -1,5 +1,5 @@
 number_of_completed_homeworks = 12
 number_of_hours_spent = 1.5
-Course_name = "Python"
-Time_for_one_task = number_of_hours_spent / number_of_completed_homeworks
-print("Курс:",Course_name,", всего задач:",number_of_completed_homeworks,", затрачено часов: ",number_of_hours_spent,", среднее время выполнения",Time_for_one_task,"часа.")
+course_name = "Python"
+time_for_one_task = number_of_hours_spent / number_of_completed_homeworks
+print("Курс:",course_name,", всего задач:",number_of_completed_homeworks,", затрачено часов: ",number_of_hours_spent,", среднее время выполнения",time_for_one_task,"часа.")
